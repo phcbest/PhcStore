03db43cc690ed56e7bcdc08935b714590a3a871c
PengHaiChen <phcbest2017@outlook.com>
2021-08-09 17:10:30 +0800

    2021-08-09 17:09:52 编写rabbit mq 单元测试

--- store-order/src/test/java/com/phc/phcstore/storeorder/StoreOrderApplicationTests.java
+++ store-order/src/test/java/com/phc/phcstore/storeorder/StoreOrderApplicationTests.java
@@ -1,13 +1,57 @@
 package com.phc.phcstore.storeorder;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.DirectExchange;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+@Slf4j
 @SpringBootTest
 class StoreOrderApplicationTests {
 
+    /***
+     * 创建Exchange，queue，并且绑定关系
+     *  使用全参构造器创建交换机
+     */
+
+    @Autowired
+    AmqpAdmin amqpAdmin;
+
     @Test
     void contextLoads() {
+        DirectExchange directExchange = new DirectExchange("hello-java-exchange", true, false);
+        amqpAdmin.declareExchange(directExchange);
+        log.info("exchange [{}] create success", "hello-java-exchange");
+
     }
 
+    @Test
+    void createQueue() {
+        Queue queue = new Queue("hello-java-queue", true, false, false);
+        amqpAdmin.declareQueue(queue);
+        log.info("create queue [{}]success", "hello-java-queue");
+
+    }
+
+    @Test
+    void createBinding() {
+        Binding binding = new Binding("hello-java-queue", Binding.DestinationType.QUEUE,
+                "hello-java-exchange", "hello.key", null);
+        amqpAdmin.declareBinding(binding);
+        log.info("交换机 {} 与队列 {} 绑定成功", "hello-java-exchange", "hello-java-queue");
+    }
+
+    @Autowired
+    RabbitTemplate rabbitTemplate;
+
+    //测试发送消息
+    @Test
+    void sendMessageTest() {
+//        rabbitTemplate.convertAndSend();
+    }
 }
--- store-product/src/main/java/com/phc/phcstore/storeproduct/StoreProductApplication.java
+++ store-product/src/main/java/com/phc/phcstore/storeproduct/StoreProductApplication.java
@@ -7,11 +7,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * 1 整合mybatis-plus
- *      1） 导入依赖 mybatis-plus-boot-starter
- *      2)  配置数据源
- *      3)  使用MapperScan 配置扫描dao层路径
- *      4） 配置扫描 xml 路径 mapper-locations
- *
+ * 1） 导入依赖 mybatis-plus-boot-starter
+ * 2)  配置数据源
+ * 3)  使用MapperScan 配置扫描dao层路径
+ * 4） 配置扫描 xml 路径 mapper-locations
  */
 @EnableDiscoveryClient
 @MapperScan("com.phc.phcstore.storeproduct.product.dao")
